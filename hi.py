49115807a757e3d10cbbc8d5bacdc1c6c7cf943c
AlexTrevathan <124939135+AlexTrevathan@users.noreply.github.com>
2023-02-14 13:57:50 +1300

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,14 +1,1 @@
-#MeanCodingMean.py
-#a discription of an awesome code
-#21/10/2021
-#Alex Trevathan
-print("discipline equals freedom")
-
-index = 1
-
-while index < 5:
-    name = input("Coders on tour #{} What is your name?".fromat(index))
-    age = int(input("How old are you?"))
-    print("if i add 20 years to your age you will be{}".format(age+20))
-index += 1
-
+print("Hello world")
